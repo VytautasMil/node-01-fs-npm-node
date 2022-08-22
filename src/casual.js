5957d6c2ef8c58d5dea8af55964f30d24b5a5ddf
Vytautas <vyckelis@gmail.com>
2022-08-22 11:44:51 +0300

    makeMeCards

--- src/casual.js
+++ src/casual.js
@@ -3,10 +3,31 @@
 const casual = require('casual');
 
 const sent1 = casual.sentence;
+
 console.log('sent1 ===', sent1);
 
 const city1 = casual.city;
+
 console.log('city1 ===', city1);
 
+// parasyti funkcija kuri grazina ir atspausdina random varda ir pavarde is casual
+function printRandomName() {
+  const randNameSurname = `${casual.first_name} ${casual.last_name}`;
+  console.log(randNameSurname);
+  return randNameSurname;
+}
+printRandomName();
 
-//parasyti funkcija kuri grazina ir atspausdina random varda ir pavarde is casual
+// parasyti funkcija kuri grazina argumentu paduota skaiciu sugeneruotu mokejimo korteliu
+// makeMeCards(4)
+function makeMeCards(howMany) {
+  const cards = [];
+  for (let i = 0; i < howMany; i++) {
+    cards.push(casual.card_data);
+  }
+  return cards;
+}
+console.log('makeMeCards(4) ===', makeMeCards(4));
+
+// parasyti funkcija kuri sugeneruoja random data metai, menuo, diena. atspausdina ir grazina
+// panaudoti sita funckija ir gauti random data app.js
