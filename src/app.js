5f0298696f68c17d12abc2edf6f522fbf166498b
Vytautas <vyckelis@gmail.com>
2022-08-22 08:52:06 +0300

    :

---
+++ src/app.js
@@ -0,0 +1,9 @@
+`use strict`;
+import {hoursToMin} from '.helper';
+console.log(`app.js`);
+
+const name = `james`;
+
+const fullName = `${name} Bond`;
+
+console.log('fullName ===', fullName);
---
+++ src/helper.js
@@ -0,0 +1,9 @@
+//sukurti funkcija kuri pavercia valandas i minutes
+
+function hoursToMin(hoursAmount) {
+    return hoursAmount * 60;
+}
+const n1 = hoursToMin(10);
+console.log('n1 ===', n1);
+
+//eksportuot ir panaudot app.js
