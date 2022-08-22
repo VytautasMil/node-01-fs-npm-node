6f6ba2de05141f750eddc9d6168fb71a13d0abdb
Vytautas <vyckelis@gmail.com>
2022-08-22 10:15:48 +0300

    unlink

--- fs.js
+++ fs.js
@@ -5,11 +5,48 @@ const fs = require('fs');
 
 // irasyti i failu sistema
 // fs.writeFile('failo pavadinimas su keliu', 'duomenys i faila', callback funkcija su klaidos objektu)
-fs.writeFile('./user.txt', 'Username=James', (err) => {
-  if (err) {
-    // ivyko klaida
-    console.log(err);
-    return;
-  }
-  console.log('failas sukurtas sekmingai');
-});
+
+function irasyt() {
+  fs.writeFile('./user.txt', 'Username=James\nage=25', (err) => {
+    if (err) {
+      // ivyko klaida
+      console.log(err);
+      return;
+    }
+    console.log('failas sukurtas sekmingai');
+  });
+}
+
+function skaitom(path) {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      // ivyko klaida
+      console.log(err.message);
+      return;
+    }
+    // klaidos nera
+    console.log('data ===', data.toString());
+    const rows = data.toString().split('\n');
+    console.log('rows ===', rows);
+  });
+}
+// skaitom('./user.txt');
+
+// istrinti faila is failu sistemos
+// fs.unlink
+
+// istrink('./delete.txt')
+
+if (fs.existsSync('./delete.txt')) {
+  console.log('file exists');
+  fs.unlink('./delete.txt', (err) => {
+    if (err) {
+      // ivyko klaida
+      console.log(err.message);
+      return;
+    }
+    console.log('file deleted successfuly');
+  });
+} else {
+  console.log('file not found');
+}
