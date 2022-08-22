57fe01c834306359b6ecac5689ae335bb69a9493
Vytautas <vyckelis@gmail.com>
2022-08-22 11:05:26 +0300

    casual

---
+++ src/casual.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const casual = require('casual');
+
+const sent1 = casual.sentence;
+console.log('sent1 ===', sent1);
+
+const city1 = casual.city;
+console.log('city1 ===', city1);
+
+
+//parasyti funkcija kuri grazina ir atspausdina random varda ir pavarde is casual
